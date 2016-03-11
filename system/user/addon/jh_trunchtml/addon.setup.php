a721f20c249e4aa390a8bff5ccd45114df5d43e7
jHards <hardisty.jc@gmail.com>
2016-03-11 15:48:10 +0000

    Create addon.setup.php

---
+++ system/user/addon/jh_trunchtml/addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'author' => 'jHards',
+    'author_url' => 'https://github.com/jHards/',
+    'description' => 'A HTML aware way to truncates HTML/Text after a specified number of characters. Thanks to Oliver Heine for providing TruncHTML. I needed one for EE3 and reproduced the same functionality from it since it was let go.',
+    'docs_url' => 'https://github.com/jHards/jh_trunchtml.ee_addon',
+    'name' => 'JH TruncHTML',
+    'settings_exist' => false,
+    'version' => '1.0.0',
+    'namespace'   => 'JH\TruncHTML'
+);
